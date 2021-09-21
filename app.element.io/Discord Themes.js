c483920387213b7766d00363f3f6f2ada07b3d3e
Oj <oj@oojmed.com>
2021-09-21 18:40:23 +0100

    [Various] Use latest CSS lib

--- app.element.io/Discord Themes.js
+++ app.element.io/Discord Themes.js
@@ -1,7 +1,7 @@
 let CSS;
 
 export const load = async () => {
-  CSS = await import('https://polyglot-mod.github.io/standard/src/css.js');
+  CSS = await import(`https://polyglot-mod.github.io/standard/src/css.js?_${Date.now()}`);
 
   const classRemap = {
     '.mx_LeftPanel': 'sidebar-2K8pFh',
--- app.revolt.chat/Discord Themes.js
+++ app.revolt.chat/Discord Themes.js
@@ -1,7 +1,7 @@
 let CSS;
 
 export const load = async () => {
-  CSS = await import('https://polyglot-mod.github.io/standard/src/css.js');
+  CSS = await import(`https://polyglot-mod.github.io/standard/src/css.js?_${Date.now()}`);
 
   const classRemap = {
     '.sc-laRQdt.eAQDAO': 'sidebar-2K8pFh',
