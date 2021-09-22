e6e134edf5c5195b1ec35fef0e09a3b1bf9ee0a4
Oj <oj@oojmed.com>
2021-09-22 08:38:06 +0100

    [Slack > Discord Themes] Add more remapping

--- app.slack.com/Discord Themes.js
+++ app.slack.com/Discord Themes.js
@@ -30,7 +30,7 @@ export const load = async () => {
       ['--header-primary', '#FFFFFF'],
       ['--text-muted', 'rgb(207, 195, 207)'],
       ['--text-muted', 'rgba(var(--sk_foreground_max_solid,97,96,97),1)'],
-      ['--header-secondary', 'rgba(var(--sk_foreground_max,29,28,29),.7)'],
+      ['--header-secondary', 'rgba(var(--sk_foreground_max,29,28,29),0.7)'],
       ['--background-accent', '#1164A3'],
       ['--text-link', 'rgba(var(--sk_highlight,18,100,163),1)']
     ].map((v) => {
