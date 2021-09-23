c8b53f16072f6fc7e74fb30b520e17376501cde5
CanadaHonk <19228318+CanadaHonk@users.noreply.github.com>
2021-09-23 13:03:27 +0100

    [Teams > Discord Themes] Initial Add

---
+++ teams.microsoft.com/DIscord Themes.js
@@ -0,0 +1,59 @@
+let CSS;
+
+export const load = async () => {
+  CSS = await import(`https://polyglot-mod.github.io/standard/src/css.js?_${Date.now()}`);
+  
+  const classRemap = {
+    /* '.mx_LeftPanel': 'sidebar-2K8pFh',
+    '.mx_GroupFilterPanel': 'wrapper-3NnKdC guilds-1SWlCJ',
+    '.mx_LeftPanel_userHeader': 'container-1taM1r clickable-25tGDB',
+    '.mx_RoomHeader_wrapper': 'title-3qD0b- container-1r6BKw themed-ANHk51',
+    '.mx_MainSplit': 'content-yTz4x3',
+    '.mx_RoomView_body': 'chatContent-a9vAAp' */
+    /* '.??': 'membersWrap-2h-GB4 hiddenMembers-2dcs_q', */
+  };
+  
+  for (const [ toMap, remap ] of Object.entries(classRemap)) {
+    document.querySelectorAll(`${toMap}`).forEach((x) => x.className += ' ' + remap);
+  }
+
+  document.body.parentElement.classList.add('theme-dark');
+  
+  for (const sheet of window.document.styleSheets) {
+    let themeVars = [
+      ['--background-tertiary', '#0a0a0a'],
+      ['--background-secondary', '#141414'],
+      ['--background-primary', '#1f1f1f'],
+      ['--background-floating', '#242424'],
+      ['--background-accent', '#292929'],
+      // ['--text-normal', '#fff'],
+      ['--interactive-hover', '#fff'],
+      ['--interactive-normal', '#adadad'],
+      ['--brand-experiment', '#9ea2ff', 'var(--brand-experiment, var(--text-link, #9ea2ff))']
+    ].map((v) => {
+      if (v[1][0] === '#') {
+        v[1] = `rgb(${parseInt(v[1].substring(1, 3), 16)}, ${parseInt(v[1].substring(3, 5), 16)}, ${parseInt(v[1].substring(5, 7), 16)})`;
+      }
+      
+      return v;
+    });
+    
+    for (let rule of sheet.cssRules) {
+      if (!rule.selectorText) continue;
+      
+      for (let v of themeVars) {
+        rule.style.cssText = rule.style.cssText.replaceAll(v[1], v[2] || `var(${v[0]}, ${v[1]})`);
+      }
+    }
+  }
+  
+  CSS.add(``);
+  
+  
+  // CSS.add(`@import "${prompt('Theme Link')}";`);
+  // CSS.add(await (await fetch(prompt('Theme Link:'))).text());
+};
+
+export const unload = () => {
+  
+};
