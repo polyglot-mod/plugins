f62c2afd446c22cea85c089bcd309dfca00fd9c8
CanadaHonk <19228318+CanadaHonk@users.noreply.github.com>
2021-09-24 12:58:00 +0100

    [Themes > Custom CSS] Fix file opening being blocked because of no user gesture

--- themes/Custom CSS.js
+++ themes/Custom CSS.js
@@ -3,9 +3,11 @@ let CSS;
 export const load = async () => {
   CSS = await import(`https://polyglot-mod.github.io/standard/src/css.js?_${Date.now()}`);
 
-  const cssFileContent = await (await (await showOpenFilePicker())[0].getFile()).text();
+  document.body.addEventListener('click', () => { // wait for click event as needs user gesture to open files
+    const cssFileContent = await (await (await showOpenFilePicker())[0].getFile()).text();
 
-  CSS.add(cssFileContent);
+    CSS.add(cssFileContent);
+  }, { once: true });
 };
 
 export const unload = () => {
