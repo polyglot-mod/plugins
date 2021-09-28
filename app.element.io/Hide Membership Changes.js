09c90ad4b200edd998a7e52564b5437d66a170bd
Oj <oj@oojmed.com>
2021-09-28 22:03:28 +0100

    [Element > Hide Membership Changes] Initial Add

---
+++ app.element.io/Hide Membership Changes.js
@@ -0,0 +1,38 @@
+let CSS, interval;
+
+const removeUnneededDates = () => {
+  const children = [...document.querySelector('.mx_RoomView_MessageList').children];
+
+  for (const el of document.querySelectorAll('.mx_RoomView_MessageList li:not([class])')) {
+    let ind = children.indexOf(el) + 1;
+
+    while (true) {
+      if (!children[ind] || children[ind].className === '') {
+        el.style.display = 'none';
+        break;
+      }
+
+      if (children[ind].className !== 'mx_EventListSummary' && children[ind].className !== '') break;
+
+      ind++;
+    }
+  }
+};
+
+export const load = async () => {
+  CSS = await import(`https://polyglot-mod.github.io/standard/src/css.js?_${Date.now()}`);
+
+  CSS.add(`.mx_EventListSummary {
+    display: none;
+  }`);
+
+  interval = setInterval(removeUnneededDates, 500);
+  removeUnneededDates();
+};
+
+export const unload = () => {
+  clearInterval(interval);
+  CSS.remove();
+
+  document.querySelectorAll('.mx_RoomView_MessageList li:not([class])').forEach((x) => x.style.display = '');
+};
