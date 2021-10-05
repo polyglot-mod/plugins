4bd3490bae519bbd6c52171c1403505807be5d81
Oj <oj@oojmed.com>
2021-10-05 14:14:04 +0100

    [Google > Theming] Tweaking and improvements

--- www.google.com/Theming.js
+++ www.google.com/Theming.js
@@ -15,17 +15,21 @@ export default new (class PluginWrapper extends Plugin {
       ['--interactive-normal', '#202124'],
       ['--interactive-normal', 'rgba(0, 0, 0, 0.87)'],
       ['--interactive-normal', '#222222'],
+      ['--interactive-normal', '#212121'],
       ['--text-muted', 'rgba(0, 0, 0, 0.54)'],
-      ['--text-muted', '#3c4043'],    ['--interactive-normal', '#212121'],
+      ['--text-muted', '#3c4043'],
       ['--text-muted', '#4d5156'],
       ['--brand-experiment', '#1a73e8'],
+      ['--brand-experiment', '#4285f4'],
+      ['--brand-experiment', 'rgb(66, 133, 244)'],
       ['--text-muted', '#5f6368'],
       ['--background-accent', '#dfe1e5'],
-      ['--interactive-normal', '#70757a'],
+      ['--text-muted', '#70757a'],
       ['--text-muted', '#9aa0a6'],
       ['--header-primary', '#000000'],
       ['--text-link', '#1a0dab'],
-      ['--background-accent', 'rgb(241, 243, 244)']
+      ['--background-floating', 'rgb(241, 243, 244)'],
+      ['--background-accent', 'rgba(218, 220, 224, 0.5)']
     ]);
     
     this.CSS.add(`/* body, .appbar {
